124915989c4bd71cc84b21008b6ad6a43b36901b
Steven <schenn1992@gmail.com>
2021-01-05 00:13:41 +0000

    add Question class

--- two_o_player.rb
+++ two_o_player.rb
@@ -10,8 +10,18 @@ class Player
     @score -= 1
   end 
 
-
 end
 
+class Question 
+  
+  def initialize()
+    @firstNum = rand(1...21)
+    @secondNum = rand(1...21)
+  end
 
+  def addition_math_question
+    puts "What does #{@firstNum} plus #{@secondNum} equal to?"
+  end
+
+end
 
